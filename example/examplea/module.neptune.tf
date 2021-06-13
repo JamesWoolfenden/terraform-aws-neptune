0ee5de5ee603749475f0fa21ee54e5f8813a93bd
James Woolfenden <James.Woolfenden@gmail.com>
2021-06-13 16:47:00 +0100

    refresh

--- example/examplea/module.neptune.tf
+++ example/examplea/module.neptune.tf
@@ -5,7 +5,7 @@ module "neptune" {
   kms_key_arn   = ""
   cluster       = local.config
   vpc_id        = element(tolist(data.aws_vpcs.main.ids), 0)
-  allowed_range = module.ip.cidr
+  allowed_range = [module.ip.cidr]
 }
 
 module "ip" {
