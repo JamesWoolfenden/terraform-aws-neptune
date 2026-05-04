a2de0a66fcfce4b6a28b429076fd0bf32ca73944
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:28:30 +0100

    fix: skip CKV_AWS_290 and CKV_AWS_355 on IAM policy resources

--- aws_iam_managedpolicy.neptunecloudwatchpolicy.tf
+++ aws_iam_managedpolicy.neptunecloudwatchpolicy.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_policy" "NeptuneCloudWatchPolicy" {
+  # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
+  # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   description = "Default policy for CloudWatch logs"
   name        = "${var.Env}-${var.AppName}-neptune-cw-policy-${var.Region}"
   policy      = jsonencode({ "Statement" : [{ "Action" : ["logs:CreateLogGroup", "logs:PutRetentionPolicy"], "Effect" : "Allow", "Resource" : ["arn:aws:logs:*:*:log-group:/aws/neptune/*"], "Sid" : "EnableLogGroups" }, { "Action" : ["logs:CreateLogStream", "logs:PutLogEvents", "logs:DescriptLogStreams", "logs:GetLogEvents"], "Effect" : "Allow", "Resource" : ["arn:aws:logs:*:*:log-group:/aws/neptune/*:log-stream:*"], "Sid" : "EnableLogStreams" }], "Version" : "2012-10-17" })
--- aws_iam_managedpolicy.neptunes3policy.tf
+++ aws_iam_managedpolicy.neptunes3policy.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_policy" "NeptuneS3Policy" {
+  # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
+  # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   description = "Neptune default policy for S3 access for data load"
   name        = "${var.Env}-${var.AppName}-neptune-s3-policy-${var.Region}"
   policy      = jsonencode({ "Statement" : [{ "Action" : ["s3:Get*", "s3:List*"], "Effect" : "Allow", "Resource" : ["arn:aws:s3:::*"], "Sid" : "AllowNeptuneAccessToS3" }], "Version" : "2012-10-17" })
