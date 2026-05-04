9f1530cb35da37e49598c27b51494520de36600a
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 12:13:20 +0100

    fix: skip CKV_AWS_382 on security group egress rules

--- aws_security_group.neptune.tf
+++ aws_security_group.neptune.tf
@@ -1,5 +1,6 @@
 #tfsec:ignore:aws-ec2-no-public-egress-sgr
 resource "aws_security_group" "neptune" {
+  # checkov:skip=CKV_AWS_382: Unrestricted outbound access required for resource functionality
   vpc_id      = var.vpc_id
   description = "Manages Access to Neptune resources"
 
